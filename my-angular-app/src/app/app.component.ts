deb7e63c7ca37e25ba99ccee081005acc2698f15
93omkarjadhav <jadhavomkar2608@gmail.com>
2025-05-09 07:31:08 +0530

    Added my-angular-app as a normal folder

---
+++ my-angular-app/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'Registration Form';
+  
+  displayname='';
+  displayAddress='';
+  displaycontact='';
+  displayemail='';
+
+  getValue(name:string, Address:string, contact:string, email:string){
+    this.displayname=name;
+    this.displayAddress=Address;
+    this.displaycontact=contact;
+    this.displayemail=email;
+
+  }
+}
